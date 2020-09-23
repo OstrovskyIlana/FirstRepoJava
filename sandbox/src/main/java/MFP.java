b8ae3cebc56d471a2e79fe834301bf4701dba2e6
OstrovskyIlana <ostrovskyilana@gmail.com>
2020-09-23 12:53:43 +0300

    fuck the world done

--- sandbox/src/main/java/MFP.java
+++ sandbox/src/main/java/MFP.java
@@ -1,5 +1,7 @@
 public class MFP {
     public static void main (String[]args) {
-     System.out.println("hello world!");
+     System.out.println("fuck the world!");
+System.out.println("hello world!");
        }
+
 }
